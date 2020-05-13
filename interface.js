4975fa5db6dfd2ea73ac16cb32a1ecbd98cd93e5
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-05-13 15:36:31 +0100

    Link jquery interface to html and js logic

--- interface.js
+++ interface.js
@@ -1,15 +1,32 @@
 $( document ).ready(function() {
+
   var thermostat = new Thermostat()
-  $("h1").text(thermostat.temperature);
+  function updateTemperature() {
+      $("h1").text(thermostat.temperature);
+  }
+  updateTemperature();
 
   $('#temperature-up').click(function(){
     thermostat.up();
-    $("h1").text(thermostat.temperature);
+    updateTemperature();
   });
 
   $('#temperature-down').click(function(){
     thermostat.down();
-    $("h1").text(thermostat.temperature);
+    updateTemperature();
   });
-
+  $('#temperature-reset').click(function() {
+    thermostat.resetTemperature();
+    updateTemperature();
+  })
+  $('#powersaving-on').click(function() {
+    thermostat.switchPowerSavingModeOn();
+    $('#power-saving-status').text('on');
+    updateTemperature();
+  })
+  $('#powersaving-off').click(function() {
+    thermostat.switchPowerSavingModeOff();
+    $('#power-saving-status').text('off');
+    updateTemperature();
+  })
 });
