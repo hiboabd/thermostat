f2425f9527d6ba7f0c53efd1e9bde3946e9285b5
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-05-13 16:38:50 +0100

    implemented ajax to select location for temperature

--- interface.js
+++ interface.js
@@ -2,10 +2,27 @@ $( document ).ready(function() {
 
   var thermostat = new Thermostat()
   function updateTemperature() {
-      $("h1").text(thermostat.temperature);
-      $("h1").attr('class', thermostat.energyUsage());
+      $("#temperature").text(thermostat.temperature);
+      $("#temperature").attr('class', thermostat.energyUsage());
   }
   updateTemperature();
+
+  function displayWeather(city){
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=057131373489e84b5395df21e3725054';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data){
+      $('#current-temperature').text(data.main.temp);
+    })
+  }
+
+  displayWeather('london');
+
+  $('#current-city').change(function(){
+    var city = $('#current-city').val();
+    displayWeather(city)
+  })
+
 
   $('#temperature-up').click(function(){
     thermostat.up();
